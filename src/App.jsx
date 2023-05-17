f38d33a99bffbe1db0ff00dac57494daab0e7cf8
WakilMahmud <wakilmahmud30@gmail.com>
2023-05-17 22:40:35 +0600

    Firebase deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 const App = () => {
 	return (
 		<div>
-			<p className="text-red-500">Hello</p>
 			<button className="btn btn-info">Info</button>
 			<button className="btn btn-success">Success</button>
 			<button className="btn btn-warning">Warning</button>
