051a640cb1bbdf1f79054470bd7f37d9435b0fcf
WakilMahmud <wakilmahmud30@gmail.com>
2023-05-18 16:13:38 +0600

    Login Page is created

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,5 +1,35 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-	return <div>Login</div>;
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const onSubmit = (data) => console.log(data);
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-1/4 mx-auto my-32 space-y-4">
+				{/* register your input into the hook by invoking the "register" function */}
+				<input defaultValue="email" placeholder="Enter your email" className="border rounded p-2" {...register("email")} />
+				<input defaultValue="password" placeholder="Enter your password" className="border rounded p-2" {...register("password")} />
+				{/* errors will return when field validation fails  */}
+				{errors.exampleRequired && <span>This field is required</span>}
+				<input className="btn btn-info" type="submit" value="Login" />
+
+				<p>
+					New User?
+					<Link to="/register">
+						<button className="btn btn-active btn-link">Register</button>
+					</Link>
+				</p>
+
+				<button className="btn btn-warning">Sign in with Google</button>
+			</form>
+		</div>
+	);
 };
 
 export default Login;
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,5 +1,32 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
 const Register = () => {
-	return <div>Register</div>;
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const onSubmit = (data) => console.log(data);
+	return (
+		<div>
+			<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-1/4 mx-auto my-32 space-y-4">
+				{/* register your input into the hook by invoking the "register" function */}
+				<input defaultValue="email" placeholder="Enter your email" className="border rounded p-2" {...register("email")} />
+				<input defaultValue="password" placeholder="Enter your password" className="border rounded p-2" {...register("password")} />
+				{/* errors will return when field validation fails  */}
+				{errors.exampleRequired && <span>This field is required</span>}
+				<input className="btn btn-info" type="submit" value="Register" />
+
+				<p>
+					Already have an account?
+					<Link to="/login">
+						<button className="btn btn-active btn-link">Login</button>
+					</Link>
+				</p>
+			</form>
+		</div>
+	);
 };
 
 export default Register;
