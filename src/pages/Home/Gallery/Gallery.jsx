c06106d5d2fdc15b6bcd1bfcaadeb9e279c5c70b
WakilMahmud <wakilmahmud30@gmail.com>
2023-05-19 01:44:56 +0600

    All Toys page is done

--- src/pages/Home/Gallery/Gallery.jsx
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -7,33 +7,33 @@ const Gallery = () => {
 		{
 			id: 1,
 			name: "Speed Racer",
-			image: "/path/to/speed-racer.jpg",
+			image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtd3vZ_ffnEn9yY3z-9Zm3-VhgNdhgBooHIQ&usqp=CAU",
 		},
 		{
 			id: 2,
 			name: "Fire Chief",
-			image: "/path/to/fire-chief.jpg",
+			image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_hq4pvSAp_hehAF6EpJgHm_4ttMaJnC5D9w&usqp=CAU",
 		},
 		{
 			id: 3,
 			name: "Monster Truck",
-			image: "/path/to/monster-truck.jpg",
+			image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJLondmk_4UFUw5L5-Q-K5gy1dbMWWMCsCDg&usqp=CAU",
 		},
 		{
 			id: 4,
 			name: "Police Pursuit",
-			image: "/path/to/police-pursuit.jpg",
+			image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUL8IX3h10SFYCjkfytnXJcH2lemOiPc-8SA&usqp=CAU",
 		},
 	];
 
 	return (
 		<section className="bg-gray-200 py-8" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-			<div className="container mx-auto">
-				<h2 className="text-3xl text-center font-bold mb-4">Toy Car Gallery</h2>
+			<div className="container mx-auto p-6">
+				<h2 className="text-3xl text-center font-bold mt-4 mb-8">Toy Car Gallery</h2>
 				<div className="grid grid-cols-2 md:grid-cols-4 gap-4">
 					{toyCars.map((toyCar) => (
 						<div key={toyCar.id} className="bg-white rounded-lg shadow-md p-4 text-center">
-							<img src={toyCar.image} alt={toyCar.name} className="w-32 mx-auto mb-4" />
+							<img src={toyCar.image} alt={toyCar.name} className="w-32 h-28 mx-auto mb-4" />
 							<h3 className="text-lg font-semibold">{toyCar.name}</h3>
 						</div>
 					))}
--- src/pages/Home/ShopCategory/ShopCategory.jsx
+++ src/pages/Home/ShopCategory/ShopCategory.jsx
@@ -1,30 +1,42 @@
+import { useEffect, useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 const ShopCategory = () => {
+	const [category, setCategory] = useState("Sports Car");
+
+	useEffect(() => {
+		// fetch(`https://toy-marketplace-server-amber.vercel.app/toys/${category}`)
+		// 	.then((res) => res.json())
+		// 	.then((data) => {
+		// 		// console.log(data);
+		// 		setCategory(data);
+		// 	});
+	}, [category]);
+
 	return (
 		<>
 			<Tabs>
 				<TabList>
 					<Tab>
-						<button className="btn btn-outline btn-accent">Sports Car</button>
+						<button className="btn btn-outline btn-accent" onClick={() => setCategory("Sports Car")}>
+							Sports Car
+						</button>
 					</Tab>
 					<Tab>
-						<button className="btn btn-outline btn-accent">Truck Car</button>
+						<button className="btn btn-outline btn-accent" onClick={() => setCategory("Truck")}>
+							Truck
+						</button>
 					</Tab>
 					<Tab>
-						<button className="btn btn-outline btn-accent">Regular Car</button>
+						<button className="btn btn-outline btn-accent" onClick={() => setCategory("Regular Car")}>
+							Regular Car
+						</button>
 					</Tab>
 				</TabList>
 
-				<TabPanel>
-					<h2>Sports Car Content</h2>
-				</TabPanel>
-				<TabPanel>
-					<h2>Truck Content</h2>
-				</TabPanel>
-				<TabPanel>
-					<h2>Regular Car Content</h2>
-				</TabPanel>
+				<TabPanel>{category.length}</TabPanel>
+				<TabPanel>{category.length}</TabPanel>
+				<TabPanel>{category.length}</TabPanel>
 			</Tabs>
 		</>
 	);
