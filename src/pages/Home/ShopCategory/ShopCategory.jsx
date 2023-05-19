ffd09336a8510176905dbbcae06ab21380f40114
WakilMahmud <wakilmahmud30@gmail.com>
2023-05-19 17:26:16 +0600

    Shop By Category Section is added

--- src/pages/Home/ShopCategory/ShopCategory.jsx
+++ src/pages/Home/ShopCategory/ShopCategory.jsx
@@ -1,44 +1,72 @@
 import { useEffect, useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import ToyCategory from "./ToyCategory";
 
 const ShopCategory = () => {
 	const [category, setCategory] = useState("Sports Car");
 
+	const [categoryData, setCategoryData] = useState([]);
+
+	const handleTabButton = (type) => {
+		setCategory(type);
+	};
+
 	useEffect(() => {
-		// fetch(`https://toy-marketplace-server-amber.vercel.app/toys/${category}`)
-		// 	.then((res) => res.json())
-		// 	.then((data) => {
-		// 		// console.log(data);
-		// 		setCategory(data);
-		// 	});
+		fetch(`https://toy-marketplace-server-amber.vercel.app/shop/${category}`)
+			.then((res) => res.json())
+			.then((data) => {
+				setCategoryData(data);
+			});
 	}, [category]);
 
+	// console.log(categoryData);
+
 	return (
-		<>
+		<div className="my-32 bg-red-200">
 			<Tabs>
 				<TabList>
-					<Tab>
-						<button className="btn btn-outline btn-accent" onClick={() => setCategory("Sports Car")}>
-							Sports Car
-						</button>
-					</Tab>
-					<Tab>
-						<button className="btn btn-outline btn-accent" onClick={() => setCategory("Truck")}>
-							Truck
-						</button>
-					</Tab>
-					<Tab>
-						<button className="btn btn-outline btn-accent" onClick={() => setCategory("Regular Car")}>
-							Regular Car
-						</button>
-					</Tab>
+					<div className="flex gap-4 px-10 pt-10">
+						<Tab>
+							<button className="btn btn-outline btn-accent" onClick={() => handleTabButton("Sports Car")}>
+								Sports Car
+							</button>
+						</Tab>
+						<Tab>
+							<button className="btn btn-outline btn-accent" onClick={() => handleTabButton("Truck")}>
+								Truck
+							</button>
+						</Tab>
+						<Tab>
+							<button className="btn btn-outline btn-accent" onClick={() => handleTabButton("Regular Car")}>
+								Regular Car
+							</button>
+						</Tab>
+					</div>
 				</TabList>
 
-				<TabPanel>{category.length}</TabPanel>
-				<TabPanel>{category.length}</TabPanel>
-				<TabPanel>{category.length}</TabPanel>
+				<TabPanel>
+					<div className="grid grid-cols-3 gap-4 p-10">
+						{categoryData.map((toy) => (
+							<ToyCategory toy={toy} key={toy._id}></ToyCategory>
+						))}
+					</div>
+				</TabPanel>
+				<TabPanel>
+					<div className="grid grid-cols-3 gap-4 p-10">
+						{categoryData.map((toy) => (
+							<ToyCategory toy={toy} key={toy._id}></ToyCategory>
+						))}
+					</div>
+				</TabPanel>
+				<TabPanel>
+					<div className="grid grid-cols-3 gap-4 p-10">
+						{categoryData.map((toy) => (
+							<ToyCategory toy={toy} key={toy._id}></ToyCategory>
+						))}
+					</div>
+				</TabPanel>
 			</Tabs>
-		</>
+		</div>
 	);
 };
 
---
+++ src/pages/Home/ShopCategory/ToyCategory.jsx
@@ -0,0 +1,58 @@
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
+import Swal from "sweetalert2";
+
+const ToyCategory = ({ toy }) => {
+	// console.log(toy);
+	const { _id, toy_picture, toy_name, price, rating } = toy;
+
+	const { user } = useContext(AuthContext);
+	const navigate = useNavigate();
+
+	const handleViewDetails = () => {
+		if (!user) {
+			Swal.fire({
+				title: "You have to log in first to view details",
+
+				showCancelButton: true,
+				confirmButtonText: "OK",
+			}).then((result) => {
+				/* Read more about isConfirmed, isDenied below */
+				if (result.isConfirmed) {
+					navigate("/login");
+				}
+			});
+		}
+	};
+
+	return (
+		<>
+			<div className="card card-compact w-96 bg-base-100 shadow-xl">
+				<figure>
+					<img src={toy_picture} alt="Toy Picture" className="h-40" />
+				</figure>
+				<div className="card-body">
+					<h2 className="card-title">{toy_name}</h2>
+					<p>Price: {price}</p>
+					<p>Rating: {rating}</p>
+					<div className="card-actions justify-center">
+						{user ? (
+							<Link to={`/toy/${_id}`}>
+								<button className="btn btn-primary" onClick={handleViewDetails}>
+									View Details
+								</button>
+							</Link>
+						) : (
+							<button className="btn btn-primary" onClick={handleViewDetails}>
+								View Details
+							</button>
+						)}
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default ToyCategory;
